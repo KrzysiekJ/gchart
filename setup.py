0eb606601e170f3514dd85fd861c52316289463f
Krzysztof Jurewicz <krzysztof.jurewicz@gmail.com>
2012-08-11 23:38:41 +0200

    setup.py fix

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 
+version = '0.0.1'
+
 setup(
     name = 'gchart',
     url = 'https://github.com/KrzysiekJ/gchart',
-    version = '0.0.1',
+    download_url = 'https://github.com/KrzysiekJ/gchart/tarball/{}'.format(version),
+    version = version,
     author = 'Krzysztof Jurewicz',
     author_email = 'krzysztof.jurewicz@gmail.com',
     description = 'A flexible Google Chart API wrapper'
